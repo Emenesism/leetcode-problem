127fd9e99e719a4928f6cf34636eb37c3765f311
emenesism <mhdnris@gmail.com>
2026-05-21 18:19:56 +0330

    feat: add remove duplicates exercise project

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var slow = 0;
+        var fast = 1;
+        while (fast < nums.Length)
+        {
+            if (nums[slow] != nums[fast])
+            {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            else
+            {
+                fast++;
+            }
+        }
+        return slow + 1;
+    }
+}

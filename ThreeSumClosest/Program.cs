960cf6fd0b8ccc13bb889eb6359e6d898e30cdae
emenesism <mhdnris@gmail.com>
2026-05-21 18:20:14 +0330

    feat: add three sum closest exercise project

---
+++ ThreeSumClosest/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            var left = i + 1;
+            var right = nums.Length - 1;
+            while (left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (Math.Abs(sum - target) < Math.Abs(closest - target))
+                {
+                    closest = sum;
+                }
+
+                if (sum < target)
+                {
+                    left++;
+                }
+                else if (sum > target)
+                {
+                    right--;
+                }
+                else
+                {
+                    return sum;
+                }
+            }
+        }
+        return closest;
+    }
+
+}

65db071ee8a2ef16c77d38c165f22422e167e873
emenesism <mhdnris@gmail.com>
2026-05-24 13:42:09 +0330

    fix bug in search range

--- SearchRange/Program.cs
+++ SearchRange/Program.cs
@@ -7,43 +7,6 @@
 
         return new int[] { first, last };
     }
-    public class Solution
-    {
-        public int[] SearchRange(int[] nums, int target)
-        {
-            if (nums.Length == 0)
-            {
-                return new[] { -1, -1 };
-            }
-            if (nums.Length == 1 && nums[0] == target)
-            {
-                return new[] { 0, 0 };
-            }
-            List<int> result = new();
-            int left = 0;
-            int right = nums.Length - 1;
-
-            while (left <= right)
-            {
-                int mid = left + (right - left) / 2;
-
-                if (nums[mid] == target)
-                {
-                    result.Add(mid);
-                }
-
-                if (nums[mid] > target)
-                {
-                    right = mid - 1;
-                }
-
-
-
-            }
-            Array.Sort(result);
-            return result;
-        }
-    }
 
     private int FindFirst(int[] nums, int target)
     {

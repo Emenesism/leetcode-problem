8ec3e27e80fcf39bead71063b9886ac074701573
emenesism <mhdnris@gmail.com>
2026-05-24 12:44:49 +0330

    feat: add remove element exercise project

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var slow = 0;
+        var fast = 0;
+        while (fast < nums.Length)
+        {
+            if (nums[fast] != val)
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}

bfce54370b338f134c3ff5f666661ece27db24b9
emenesism <mhdnris@gmail.com>
2026-05-24 15:28:10 +0330

    feat: add first missing positive exercise project

---
+++ FirstMissingPositive/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            while (1 <= nums[i] && nums[i] <= nums.Length && nums[nums[i] - 1] != nums[i])
+            {
+                (nums[i], nums[nums[i] - 1]) = (nums[nums[i] - 1], nums[i]);
+            }
+        }
+
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (nums[j] != j + 1)
+            {
+                return j + 1;
+            }
+        }
+
+        return nums.Length + 1;
+    }
+}
